29a726538a49f8326708aa8b6278400f64d14e8c
habib sadullaev <h.sadullaev@gmail.com>
2020-11-16 15:25:57 +0500

    add string comparison with 'STARTS WITH', 'CONTAINS', 'ENDS WITH' operators

--- src/EQL/Comparison.fs
+++ src/EQL/Comparison.fs
@@ -1,11 +1,30 @@
 ﻿module EQL.Comparison
 
+open FSharp.Quotations
 open FParsec
+open TypeShape.Core
 open TypeShape.Core.StagingExtensions
+open EQL.Constant
 open EQL.PropertyChain
 
-let mkComparison param =
-    parse { let! res = mkPropChain param
-            if res.Type = typeof<bool> 
-            then return  Expr.cast<bool> res 
-            else return! unsupported res.Type }
+let stringComparison () : Parser<Expr<string -> string -> bool>, 'u> =
+    choice [
+        stringCIReturn "STARTS WITH" <@ fun x y -> x.StartsWith y @>
+        stringCIReturn "CONTAINS"    <@ fun x y -> x.Contains   y @>
+        stringCIReturn "ENDS WITH"   <@ fun x y -> x.EndsWith   y @>
+    ] .>> spaces
+
+let rec mkComparison param =
+    mkPropChain param >>= mkComparisonAux
+
+and mkComparisonAux prop =
+    match TypeShape.Create prop.Type with
+    | Shape.Bool -> preturn ^ Expr.cast prop
+
+    | Shape.String ->
+        parse { let lhs = Expr.cast prop
+                let! cmp = stringComparison()
+                let! rhs = mkConst()
+                return <@ (%cmp) %lhs %rhs @> }
+
+    | _ -> unsupported prop.Type
--- tests/EQL.Tests/ComparisonTests.fs
+++ tests/EQL.Tests/ComparisonTests.fs
@@ -21,6 +21,18 @@ let inline failedWith expected input =
 let tests =
     testList "comparison parser" [
         "Parent.Parent.HasValue" |> parsedInto <@ (%param).Parent.Parent.HasValue @>
+        
+        "String starts with 'aaa'"  |> parsedInto <@ (%param).String.StartsWith %(constExpr "aaa") @>
+        "String ContainS 'bbb'"     |> parsedInto <@ (%param).String.Contains   %(constExpr "bbb") @>
+        "String ENDS WITH 'ccc'"    |> parsedInto <@ (%param).String.EndsWith   %(constExpr "ccc") @>
 
         "Parent.HasValue = true" |> failedWith { position = 17L; message = "end of input" }
+        
+        "String contain 'zzz'" 
+        |> failedWith { 
+            position = 8L
+            message = "'CONTAINS' (case-insensitive), 'ENDS WITH' (case-insensitive) or
+'STARTS WITH' (case-insensitive)"
+        }
+
     ]
