3bc82b4930d40442c62c9490a07a55c7241387ab
habib sadullaev <h.sadullaev@gmail.com>
2020-11-18 11:08:11 +0500

    refactor NullableComparison a bit

--- src/Equal/CustomShapes.fs
+++ src/Equal/CustomShapes.fs
@@ -5,20 +5,20 @@ open TypeShape.Core
     
 type INullableComparisonVisitor<'R> =
     abstract member Visit<'t when 't : (new : unit -> 't)
-                                and 't :> ValueType
-                                and 't : struct
-                                and 't : comparison> : unit -> 'R
+                              and 't :> ValueType
+                              and 't : struct
+                              and 't : comparison> : unit -> 'R
         
 type IShapeNullableComparison =
     abstract member Element : TypeShape
     abstract member Accept : INullableComparisonVisitor<'R> -> 'R
         
 type private ShapeNullableComparison<'T when 'T : (new : unit -> 'T) 
-                                            and 'T :> ValueType 
-                                            and 'T : struct 
-                                            and 'T : comparison>() =
+                                         and 'T :> ValueType 
+                                         and 'T : struct 
+                                         and 'T : comparison>() =
     interface IShapeNullableComparison with
-        override val Element = shapeof<'T> :> TypeShape
+        override _.Element = shapeof<'T> :> TypeShape
         override _.Accept (v: INullableComparisonVisitor<'R>) = v.Visit<'T>()
     
 [<RequireQualifiedAccess>]
